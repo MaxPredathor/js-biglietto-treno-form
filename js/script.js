5dffc034023f400018763463553fd638ed66ea3b
MaxPredathor <max.gilli98@gmail.com>
2023-10-10 14:00:55 +0200

    html structure

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let costoTotale
+
+const tariffa = 0.21;
+const scontoVenti = 20/100;
+const scontoQuaranta = 40/100
+
+// let km = prompt('Inserisci i kilometri che devi percorrere')
+
+// if(parseInt(km)){  
+//  }else{
+//      alert('Questo non è un kilometraggio valido, reinserire il valore')
+//      window.location.reload()
+// }
+
+// let età = prompt('Insereisci la tua età');
+
+// if(parseInt(età)){  
+// }else{
+//     alert('Questo non è un valore valido, reinserire il valore corretto')
+//     window.location.reload()
+// }
+
+// let costo = (km * tariffa);
+// console.log(costo);
+
+// if(età < 18){
+//     costoTotale = costo - (costo * scontoVenti);
+
+// }else if (età >= 65){
+//     costoTotale = costo - (costo * scontoQuaranta);
+
+// }else {
+//     costoTotale = costo
+// }
+
+// document.getElementById('text').innerHTML = `
+// ${costoTotale.toFixed(2)}
+// `
